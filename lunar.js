d0ed2c20dff24b27308df61a34e88d948bee8904
hilmanystrom <nyhi22bc@student.ju.se>
2023-03-08 11:02:58 +0100

    final changes

--- lunar.js
+++ lunar.js
@@ -1,41 +1,63 @@
 background(195, 170, 250);
 
-// let starX = [];
-// let starY = [];
-// let starAlpha = [];
+// planet & landing plattform//
+function planet(planetX, planetY) {
+  push();
 
-// for (let i = 0; 1 < 20; i++) {
-//   const x = Math.floor(Math.random() * width);
-//   const y = Math.floor(Math.random() * height);
-//   const alpha = Math.random();
-
-//   starX.push(x);
-//   starY.push(y);
-//   starAlpha.push(alpha);
-
-//   console.log(starX);
-// }
-
-// planet //
-fill(50, 50, 50);
-ellipse(300, 620, 1000, 450);
-
-// landing platform //
-fill(60, 80, 60);
-ellipse(440, 440, 200, 80);
-fill(70, 90, 70);
-ellipse(440, 430, 180, 60);
+  translate(planetX, planetY);
+  fill(50, 50, 50);
+  ellipse(300, 550, 1000, 450);
+  fill(60, 80, 60);
+  ellipse(440, 380, 200, 80);
+  fill(70, 90, 70);
+  ellipse(440, 370, 180, 60);
+  pop();
+}
 
 // spaceship //
-push();
-translate(0, 10);
-fill(0, 0, 0);
-ellipse(80, 95, 10, 20);
-ellipse(105, 100, 10, 20);
-ellipse(175, 100, 10, 20);
-ellipse(200, 95, 10, 20);
-fill(207, 150, 150, 150);
-ellipse(140, 55, 75, 85);
-fill(135, 210, 250);
-ellipse(140, 80, 160, 40);
-pop();
+function spaceShip(spaceShipX, spaceShipY, spaceShipS) {
+  push();
+  translate(spaceShipX, spaceShipY);
+  scale(spaceShipS);
+  push();
+  translate(0, 5);
+  fill(240, 100, 255);
+  ellipse(80, 95, 10, 20);
+  ellipse(105, 100, 10, 20);
+  ellipse(175, 100, 10, 20);
+  ellipse(200, 95, 10, 20);
+  fill(207, 150, 150);
+  ellipse(140, 55, 75, 85);
+  fill(135, 210, 250);
+  ellipse(140, 80, 160, 40);
+  pop();
+  pop();
+}
+
+let starX = [];
+let starY = [];
+let starAlpha = [];
+
+for (let i = 0; i < 400; i++) {
+  const x = Math.floor(Math.random() * width);
+  const y = Math.floor(Math.random() * height);
+  const alpha = Math.random();
+
+  starX.push(x);
+  starY.push(y);
+  starAlpha.push(alpha);
+}
+
+function draw() {
+  noStroke();
+  background(195, 170, 250);
+
+  for (let index in starX) {
+    fill(255, 255, 255, Math.abs(Math.sin(starAlpha[index])) * 255);
+    ellipse(starX[index], starY[index], 3);
+    starAlpha[index] = starAlpha[index] + 0.02;
+  }
+  stroke(0);
+  spaceShip(0, 0, 1);
+  planet(0, 0, 1);
+}
--- spaceship.js
+++ spaceship.js
@@ -1,3 +1,60 @@
-ellipse(250, 155, 94, 100);
+// ellipse(250, 155, 94, 100);
 
-ellipse(250, 190, 200, 50);
+// ellipse(250, 190, 200, 50);
+
+background(255, 255, 255);
+function spaceShip(spaceShipX, spaceShipY, rotation, spaceShipS) {
+  push();
+  translate(spaceShipX, spaceShipY);
+  scale(spaceShipS);
+  clear();
+  push();
+  translate(0, 5);
+  rotate(rotation);
+  fill(240, 100, 255);
+  ellipse(80, 95, 10, 20);
+  ellipse(105, 100, 10, 20);
+  ellipse(175, 100, 10, 20);
+  ellipse(200, 95, 10, 20);
+  fill(207, 150, 150);
+  ellipse(140, 55, 75, 85);
+  fill(135, 210, 250);
+  ellipse(140, 80, 160, 40);
+  pop();
+  pop();
+}
+
+let spaceShipX = 100;
+let spaceShipY = 100;
+let rotation = 0;
+let speed = 0;
+
+function draw() {
+  spaceShip(spaceShipX, spaceShipY, rotation);
+
+  spaceShipX = spaceShipX + Math.sin(rotation) * speed;
+  spaceShipY = spaceShipY + Math.cos(rotation) * speed;
+
+  if (keyIsDown(40)) {
+    speed = 5;
+  } else if (keyIsDown(38)) {
+    speed = -5;
+  } else {
+    speed = 0;
+  }
+  if (keyIsDown(39)) {
+    rotation = rotation - 0.05;
+  } else if (keyIsDown(37)) {
+    rotation = rotation + 0.05;
+  }
+}
+
+// function spaceShip(x, y, rotation) {
+//   push();
+//   translate(x, y);
+//   rotate(rotation);
+// }
+
+// function draw() {
+//   spaceShip(100, 100, 0);
+// }
